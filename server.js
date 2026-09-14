2517cf6bd221f086904d0122f5d5e0818d90d4a6
sainaihirwe-cmd <sainaihirwe@gmail.com>
2026-09-14 09:26:16 +0100

    feat:added new features

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5050;
 
 app.use(
   cors({
-    origin: contact-fronte.netlify.app,
+    origin: "https://contact-fronte.netlify.app",
     credentials: true,
   })
 );
